f8e89dd047df8db2313f6257e81e2f72d8f4707b
GaleanoSantiago <104083617+GaleanoSantiago@users.noreply.github.com>
2022-12-20 14:45:26 -0300

    cambio de ubicacion script formspree

--- js/n7.js
+++ js/n7.js
@@ -186,3 +186,25 @@ window.addEventListener("orientationchange", ()=> {
         });
     
 });
+
+// ----------------------- Codigo para evitar ingresar a la pagina de Formspree ---------------------------
+
+const $form = document.querySelector('#contactform');
+
+$form.addEventListener('submit', handleSubmit);
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+        const form = new FormData(this);
+        const response = await fetch(this.action, {
+           method: this.method,
+           body: form,
+           headers: {
+                'Accept': 'application/json'
+           }
+            });
+        if (response.ok) {
+           this.reset();
+           alert('Su correo ha sido enviado');
+        }
+     }
